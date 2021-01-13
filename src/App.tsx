84826d188c5e84c6addb84e65add8c2368199d17
sudo-minoru <sudo.minoru@gmail.com>
2021-01-13 23:21:47 +0900

    大会検索ボタンに semantic ui を適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import RaceSearchButton from "./components/atoms/button/RaceSearchButton";
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
 function App() {
--- src/components/atoms/attoms.stories.js
+++ src/components/atoms/attoms.stories.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
-
+import "semantic-ui-css/semantic.min.css";
 import RaceSearchButton from "./button/RaceSearchButton";
 
 export const button = {
